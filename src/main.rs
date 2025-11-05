7a4440738a2ca70230fafcdfa040d73d2d6f7a12
IsmaelJC <ismael@bemmbo.com>
2025-11-05 20:06:46 -0300

    Responding to multiple PING's

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,7 +14,21 @@ fn main() {
         match stream_result {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream.write(b"+PONG\r\n").unwrap();
+
+                let mut buf: [u8; 512] = [0; 512];
+
+                loop {
+                    match stream.read(&mut buf) {
+                        Ok(num_bytes) if num_bytes > 0 => {
+                            println!("Received {} bytes from client", num_bytes);
+                            stream.write(b"+PONG\r\n").unwrap_or_else(|_| {
+                                println!("Error writing to TCP stream");
+                                0
+                            });
+                        }
+                        _ => break,
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
